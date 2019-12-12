c8af1d5184bb082164667357a2767610d6366f2c
Micheal Koblinski <stevenkoblinskip@gmail.com>
2019-12-12 17:37:18 -0500

    bootstrap styling with 4 input field

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <registration/>
+    <div class="header">
+      <h1>Registrator Here</h1>
+    </div>
+    <div class="box">
+      <registration></registration>
+    </div>
   </div>
 </template>
 
@@ -15,6 +20,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+div.box {
+  width: 800px;
+  border: 2px solid darkblue;
+  padding: 60px;
+  margin-top: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  box-shadow: 10px 10px grey
+}
+div.header {
+  text-align: center;
+  padding: 20px;
+  text-shadow: 1px 1px 3px blue;
+}
 </style>
--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,3 +1,67 @@
 <template>
-  <div>Hello World</div>
+  <div>
+    <form>
+      <div class="form-group">
+        <label for="inputUsername">Username</label>
+        <input type="text" 
+        class="form-control" 
+        id="inputUsername"
+        placeholder="Enter Username" 
+        @focus="focusUsername = true"
+        @blur="focusUsername = false">
+        <small v-if="focusUsername">Must be 4-16 characters and no spaces</small>
+      </div>
+      <div class="form-group">
+        <label for="inputEmail">Email address</label>
+        <input type="email" 
+        class="form-control" 
+        id="inputEmail"
+        placeholder="Enter email" 
+        @focus="focusEmail = true"
+        @blur="focusEmail = false">
+        <small v-if="focusEmail">Must be a real email like example@xxx.xxx</small>
+      </div>
+      <div class="form-group">
+        <label for="inputPassword">Password</label>
+        <input type="password" 
+        class="form-control" 
+        id="inputPassword" 
+        placeholder="Password"
+        @focus="focusPassword = true"
+        @blur="focusPassword = false">
+        <small v-if="focusPassword">6 to 24 characters</small>
+      </div>
+      <div class="form-group">
+        <label for="confirmPassword">Confirm Password</label>
+        <input type="password" 
+        class="form-control" 
+        id="confirmPassword" 
+        placeholder="Password">
+      </div>
+      <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      focusUsername: false,
+      focusEmail: false,
+      focusPassword: false
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
