123b1c0ff141ac73b0cd63a8328d5dfcbb964abc
Micheal Koblinski <stevenkoblinskip@gmail.com>
2019-12-12 17:38:44 -0500

    removed unnessary items

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -36,11 +36,7 @@
         <input type="password" 
         class="form-control" 
         id="confirmPassword" 
-        placeholder="Password">
-      </div>
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        placeholder="Confirm Password">
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
