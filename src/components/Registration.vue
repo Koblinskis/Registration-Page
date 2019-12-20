f27b5ff33f977dbf526a071978d85ba46117d9cb
Micheal Koblinski <stevenkoblinskip@gmail.com>
2019-12-20 11:23:45 -0500

    a

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -6,7 +6,9 @@
         <input type="text" 
         class="form-control" 
         id="inputUsername"
-        placeholder="Enter Username" 
+        placeholder="Enter Username"
+        v-model="inputName"
+        @keyup="checkUsername"
         @focus="focusUsername = true"
         @blur="focusUsername = false">
         <small v-if="focusUsername">Must be 4-16 characters and no spaces</small>
@@ -17,6 +19,8 @@
         class="form-control" 
         id="inputEmail"
         placeholder="Enter email" 
+        v-model="inputEmail"
+        @keyup="checkEmail"
         @focus="focusEmail = true"
         @blur="focusEmail = false">
         <small v-if="focusEmail">Must be a real email like example@xxx.xxx</small>
@@ -27,6 +31,8 @@
         class="form-control" 
         id="inputPassword" 
         placeholder="Password"
+        v-model="inputPassword"
+        @keyup="samePassword"
         @focus="focusPassword = true"
         @blur="focusPassword = false">
         <small v-if="focusPassword">6 to 24 characters</small>
@@ -36,9 +42,12 @@
         <input type="password" 
         class="form-control" 
         id="confirmPassword" 
-        placeholder="Confirm Password">
+        placeholder="Confirm Password"
+        v-model="inputConfirmPassword"
+        @keyup="samePassword">
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary"
+      v-if="correctEmail && correctPassword && correctUsername">Submit</button>
     </form>
   </div>
 </template>
@@ -49,11 +58,51 @@ export default {
     return {
       focusUsername: false,
       focusEmail: false,
-      focusPassword: false
+      focusPassword: false,
+      inputName: ' ',
+      inputEmail: '',
+      inputPassword: '',
+      inputConfirmPassword: '',
+      correctPassword: false,
+      correctUsername: false,
+      correctEmail: false,
+      correctLetter: false,
+      uLetter: [],
+      eLetter: [],
     }
   },
   methods: {
-    
+    samePassword() {
+      if(this.inputPassword === this.inputConfirmPassword) {
+        this.correctPassword = true
+      } else {
+        this.correctPassword = false
+      }
+    },
+    checkUsername() {
+      this.uLetter = this.inputName.split('')
+      if(this.uLetter.length > 4 && this.uLetter.length < 16) {
+        this.uLetter.forEach(element => {
+          if(element.charCodeAt(0) >= 33 && element.charCodeAt(0) <= 126){
+            this.correctUsername = true
+          } else {
+            return this.correctUsername = false
+          }
+        })
+      }
+    },
+    checkEmail() {
+      this.eLetter = this.inputEmail.split('')
+      if(this.eLetter.length !== 0) {
+        this.eLetter.forEach(element => {
+          if(element.charCodeAt(0) !== 64){
+            this.correctEmail = false
+          } else {
+            return this.correctEmail = true
+          }
+        })
+      }
+    }
   }
 }
 </script>
